e7389369133125c6f0ccb5bf2cc0d6cad6e5babd
Cash1232 <armanshash14@gmail.com>
2026-05-21 12:26:32 +1000

    module

---
+++ blox-fruits-skills.lua
@@ -0,0 +1,923 @@
+return {
+	["Rocket-Rocket"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 20,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 50,
+			Hold = 0,
+		},
+		V = {
+			Enabled = false,
+			Level = 1,
+			Hold = 0,
+		},
+	},
+	["Spin-Spin"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 2,
+		},
+		X = {
+			Enabled = true,
+			Level = 50,
+			Hold = 2,
+		},
+		C = {
+			Enabled = true,
+			Level = 90,
+			Hold = 0,
+		},
+		V = {
+			Enabled = false,
+			Level = 1,
+			Hold = 0,
+		},
+	},
+	["Blade-Blade"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 20,
+			Hold = 3,
+		},
+		C = {
+			Enabled = true,
+			Level = 50,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+	},
+	["Spring-Spring"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 20,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 50,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 125,
+			Hold = 0,
+		},
+	},
+	["Bomb-Bomb"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 50,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 75,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 125,
+			Hold = 1,
+		},
+	},
+	["Smoke-Smoke"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 20,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 50,
+			Hold = 0,
+		},
+		V = {
+			Enabled = false,
+			Level = 1,
+			Hold = 0,
+		},
+	},
+	["Spike-Spike"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 2,
+		},
+		X = {
+			Enabled = true,
+			Level = 25,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 50,
+			Hold = 3,
+		},
+		V = {
+			Enabled = true,
+			Level = 75,
+			Hold = 0,
+		},
+	},
+	["Flame-Flame"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 25,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 50,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 100,
+			Hold = 1.5,
+		},
+	},
+	["Ice-Ice"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 25,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 50,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 100,
+			Hold = 0,
+		},
+	},
+	["Sand-Sand"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 40,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 80,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 120,
+			Hold = 0,
+		},
+	},
+	["Dark-Dark"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 1,
+		},
+		X = {
+			Enabled = true,
+			Level = 40,
+			Hold = 1,
+		},
+		C = {
+			Enabled = true,
+			Level = 80,
+			Hold = 1,
+		},
+		V = {
+			Enabled = true,
+			Level = 110,
+			Hold = 2,
+		},
+	},
+	["Eagle-Eagle"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 35,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 80,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 110,
+			Hold = 0,
+		},
+	},
+	["Diamond-Diamond"] = {
+		Before = {
+			Enabled = true,
+			Skill = "Z",
+			Level = 1,
+			Hold = 0,
+		},
+		Z = {
+			Enabled = false,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 1,
+			Hold = 1.2,
+		},
+		C = {
+			Enabled = true,
+			Level = 80,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 110,
+			Hold = 0,
+		},
+	},
+	["Light-Light"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 40,
+			Hold = 3,
+		},
+		C = {
+			Enabled = true,
+			Level = 80,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 110,
+			Hold = 0,
+		},
+	},
+	["Rubber-Rubber"] = {
+		Z = {
+			Enabled = true,
+			Level = 25,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 50,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 125,
+			Hold = 0,
+		},
+		V = {
+			Enabled = false,
+			Level = 225,
+			Hold = 0,
+		},
+	},
+	["Ghost-Ghost"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 50,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 100,
+			Hold = 3,
+		},
+		V = {
+			Enabled = true,
+			Level = 150,
+			Hold = 0,
+		},
+	},
+	["Magma-Magma"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 45,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 95,
+			Hold = 0,
+		},
+		V = {
+			Enabled = false,
+			Level = 140,
+			Hold = 2,
+		},
+	},
+	["Quake-Quake"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 50,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 100,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 150,
+			Hold = 0,
+		},
+	},
+	["Buddha-Buddha"] = {
+		Before = {
+			Enabled = true,
+			Skill = "Z",
+			Level = 1,
+			Hold = 0,
+		},
+		Z = {
+			Enabled = false,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 100,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 200,
+			Hold = 0,
+		},
+	},
+	["Love-Love"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 50,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 150,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 200,
+			Hold = 0,
+		},
+	},
+	["Rocket-Rocket"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 45,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 90,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 130,
+			Hold = 0,
+		},
+	},
+	["Spider-Spider"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 50,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 100,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 225,
+			Hold = 0,
+		},
+	},
+	["Sound-Sound"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 50,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 100,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 250,
+			Hold = 0,
+		},
+	},
+	["Phoenix-Phoenix"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 90,
+			Hold = 3,
+		},
+		C = {
+			Enabled = true,
+			Level = 160,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 250,
+			Hold = 0,
+		},
+	},
+	["Portal-Portal"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 50,
+			Hold = 0,
+		},
+		C = {
+			Enabled = false,
+			Level = 50,
+			Hold = 0,
+		},
+		V = {
+			Enabled = false,
+			Level = 1,
+			Hold = 0,
+		},
+	},
+	["Lightning-Lightning"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 90,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 160,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 250,
+			Hold = 0,
+		},
+	},
+	["Pain-Pain"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 100,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 200,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 300,
+			Hold = 0,
+		},
+	},
+	["Blizzard-Blizzard"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 90,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 160,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 250,
+			Hold = 0,
+		},
+	},
+	["Gravity-Gravity"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 100,
+			Hold = 2,
+		},
+		C = {
+			Enabled = true,
+			Level = 200,
+			Hold = 3,
+		},
+		V = {
+			Enabled = true,
+			Level = 300,
+			Hold = 0,
+		},
+	},
+	["Mammoth-Mammoth"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 50,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 100,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 300,
+			Hold = 0,
+		},
+	},
+	["T-Rex-T-Rex"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 50,
+			Hold = 2,
+		},
+		C = {
+			Enabled = true,
+			Level = 100,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 300,
+			Hold = 0,
+		},
+	},
+	["Dough-Dough"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 100,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 200,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 300,
+			Hold = 3.5,
+		},
+	},
+	["Shadow-Shadow"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 100,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 200,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 300,
+			Hold = 0,
+		},
+	},
+	["Venom-Venom"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 1,
+		},
+		X = {
+			Enabled = true,
+			Level = 100,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 200,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 300,
+			Hold = 0,
+		},
+	},
+	["Gas-Gas"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 100,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 200,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 300,
+			Hold = 0,
+		},
+	},
+	["Spirit-Spirit"] = {
+		Click = true,
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 150,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 250,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 350,
+			Hold = 4,
+		},
+	},
+	["Tiger-Tiger"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 50,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 100,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 300,
+			Hold = 0,
+		},
+	},
+	["Yeti-Yeti"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 50,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 100,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 300,
+			Hold = 0,
+		},
+	},
+	["Kitsune-Kitsune"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 50,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 100,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 300,
+			Hold = 0,
+		},
+	},
+	["Control-Control"] = {
+		Before = {
+			Enabled = true,
+			Skill = "Z",
+			Level = 1,
+			Hold = 0,
+		},
+		Z = {
+			Enabled = false,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 150,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 250,
+			Hold = 0,
+		},
+		V = {
+			Enabled = true,
+			Level = 300,
+			Hold = 0,
+		},
+	},
+	["Dragon-Dragon"] = {
+		Z = {
+			Enabled = true,
+			Level = 1,
+			Hold = 0,
+		},
+		X = {
+			Enabled = true,
+			Level = 150,
+			Hold = 0,
+		},
+		C = {
+			Enabled = true,
+			Level = 250,
+			Hold = 5,
+		},
+		V = {
+			Enabled = true,
+			Level = 350,
+			Hold = 0,
+		},
+	},
+}
